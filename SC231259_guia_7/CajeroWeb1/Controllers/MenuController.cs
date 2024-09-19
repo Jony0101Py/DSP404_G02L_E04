8f2287ef86270ac75b859b3cb07c085747bda443
Ezequieel <rolandoezequiel45@gmail.com>
2024-09-18 21:23:52 -0600

    Agregando ejercicio N°1 a la guía N°7

---
+++ SC231259_guia_7/CajeroWeb1/Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CajeroWeb1.Models;
+
+namespace CajeroWeb1.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Menu(string sNumeroTarjeta, double sSaldo)
+        {
+            ViewBag.sNumeroTarjeta = sNumeroTarjeta;
+            ViewBag.sSaldo = sSaldo;
+            return View();
+        }
+    }
+}
---
+++ SC231259_guia_7/CajeroWeb1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CajeroWeb1.MvcApplication" Language="C#" %>
